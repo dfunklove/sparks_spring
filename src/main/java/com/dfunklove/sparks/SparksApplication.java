c3f67d80c65f672e62190c5836d806630eb4ce3d
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-04-26 15:59:01 -0500

    Add constant

--- src/main/java/com/dfunklove/sparks/SparksApplication.java
+++ src/main/java/com/dfunklove/sparks/SparksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SparksApplication {
+	public static final int MAX_GOALS_PER_STUDENT = 3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SparksApplication.class, args);
